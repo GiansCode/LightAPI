cf23a63bcd666b29988d1f72bad4bc676ba0c661
Obadiah Crowe <me@obadiah.io>
2019-06-11 00:42:17 +0100

    Adding comments to main api class

--- src/main/java/io/obadiah/light/LightAPI.java
+++ src/main/java/io/obadiah/light/LightAPI.java
@@ -10,6 +10,13 @@ import javax.annotation.concurrent.Immutable;
 @Immutable
 public interface LightAPI {
 
+    /**
+     * Obtains a LightAPI instance depending on your Minecraft server version.
+     *
+     * @return The corresponding LightAPI instance, as determined by your Minecraft server version.
+     *
+     * @throws IllegalStateException If the server instance is not supported by LightAPI.
+     */
     static LightAPI get() {
         switch (SpigotVersion.getServerVersion()) {
             case v1_8:
@@ -28,35 +35,95 @@ public interface LightAPI {
                 return new io.obadiah.light.nms.v1_14.LightHandler();
         }
 
-        throw new IllegalStateException(SpigotVersion.BUKKIT_VERSION + " is unsupported by LightPlugin.");
+        throw new IllegalStateException(SpigotVersion.BUKKIT_VERSION + " is unsupported by LightAPI.");
     }
 
+    /**
+     * Sets the light level of a specific block.
+     *
+     * @param world World enclosing the block.
+     * @param x X-axis of the block.
+     * @param y Y-axis of the block.
+     * @param z Z-axis of the block.
+     * @param lightLevel The light level to set the block to.
+     */
     void setLight(World world, int x, int y, int z, int lightLevel);
 
+    /**
+     * Sets the light level of a specific block.
+     *
+     * @param world World enclosing the block.
+     * @param vector Vector of the block.
+     * @param lightLevel The light level to set the block to.
+     */
     default void setLight(World world, Vector vector, int lightLevel) {
         this.setLight(world, vector.getBlockX(), vector.getBlockY(), vector.getBlockZ(), lightLevel);
     }
 
+    /**
+     * Sets the light level of a specific block.
+     *
+     * @param location Location of the block.
+     * @param lightLevel The light level to set the block to.
+     */
     default void setLight(Location location, int lightLevel) {
         this.setLight(location.getWorld(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), lightLevel);
     }
 
+    /**
+     * Removes all light from a specific block.
+     *
+     * @param world World enclosing the block.
+     * @param x X-axis of the block.
+     * @param y Y-axis of the block.
+     * @param z Z-axis of the block.
+     */
     void removeLight(World world, int x, int y, int z);
 
+    /**
+     * Removes all light from a specific block.
+     *
+     * @param world World enclosing the block.
+     * @param vector Vector of the block.
+     */
     default void removeLight(World world, Vector vector) {
         this.removeLight(world, vector.getBlockX(), vector.getBlockY(), vector.getBlockZ());
     }
 
+    /**
+     * Removes all light from a specific block.
+     *
+     * @param location Location of the block.
+     */
     default void removeLight(Location location) {
         this.removeLight(location.getWorld(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
     }
 
+    /**
+     * Forces a block light update on the specific block
+     *
+     * @param world World enclosing the block.
+     * @param x X-axis of the block.
+     * @param y Y-axis of the block.
+     * @param z Z-axis of the block.
+     */
     void updateLight(World world, int x, int y, int z);
 
+    /**
+     * Forces a block light update on the specific block
+     *
+     * @param world World enclosing the block.
+     * @param vector Vector of the block.
+     */
     default void updateLight(World world, Vector vector) {
         this.updateLight(world, vector.getBlockX(), vector.getBlockY(), vector.getBlockZ());
     }
 
+    /**
+     * Forces a block light update on the specific block
+     *
+     * @param location Location of the block.
+     */
     default void updateLight(Location location) {
         this.updateLight(location.getWorld(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
     }
